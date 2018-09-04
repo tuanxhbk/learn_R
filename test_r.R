2700e1cae8d0944c34a4a23fde8d4a8875fdaab0
Tuan Nguyen <tuan.nguyen@alibaba-inc.com>
2018-09-04 23:10:13 +0700

    Add test file

---
+++ test_r.R
@@ -0,0 +1,13 @@
+x=seq(-pi,pi,length=50)
+y=x
+f=outer(x,y,function(x,y)cos(y)/(1+x^2))
+fa=(f-t(f))/2
+contour(x,y,f)
+contour(x,y,f,nlevels=45,add=T)
+contour(x,y,fa,nlevels=15)
+image(x,y,fa)
+persp(x,y,fa)
+persp(x,y,fa,theta=30)
+persp(x,y,fa,theta=30,phi=20)
+persp(x,y,fa,theta=30,phi=70)
+persp(x,y,fa,theta=30,phi=40)
